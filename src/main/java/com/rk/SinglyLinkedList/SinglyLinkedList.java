cc1fede68a001e1b7445d8a800f7a385fb9d5368
RK <rad.krishna2001@gmail.com>
2022-02-17 23:22:57 -0500

    commit from ide coding errors - static classes if you want to use in main - missed doublequotes in print - maintain head node always after calling methods, as head node might change.

--- src/main/java/com/rk/SinglyLinkedList/SinglyLinkedList.java
+++ src/main/java/com/rk/SinglyLinkedList/SinglyLinkedList.java
@@ -1,9 +1,40 @@
 package com.rk.SinglyLinkedList;
 
 public class SinglyLinkedList {
+    private Node head=null;
+    private static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public static Node addNodeAtHead(int data, Node head){
+        if(head == null) {
+            head = new Node(data);
+        }else{
+            Node newHead = new Node(data);
+            newHead.next = head;
+            head = newHead;
+        }
+        return head;
+    }
 
 
+    public static void printSinglyLinkedList(Node head){
+        while(head!=null){
+            System.out.print(head.data+"->");
+            head = head.next;
+        }
+        System.out.print("null\n");
+    }
     public static void main(String[] args) {
-        System.out.println("hello");
+        Node head = addNodeAtHead(1,null);
+        head = addNodeAtHead(2,head);
+        head = addNodeAtHead(3,head);
+        head = addNodeAtHead(4,head);
+        printSinglyLinkedList(head);
     }
 }
